b5a738a32d2fe647181c1c2376ca20890a200b52
Pin-Xuan She <snakeneedy@gmail.com>
2024-04-29 13:48:30 +0800

    Fix import in 'problems/2215/Lc2215.java'

--- problems/2215/Lc2215.java
+++ problems/2215/Lc2215.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Lc2215 {
   static Lc2215 leetcode = new Lc2215();
 
