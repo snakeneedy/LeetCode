189c1660625e91fec4bc024d74467413198135a2
Pin-Xuan She <snakeneedy@gmail.com>
2024-05-02 20:45:52 +0800

    arraysEqual and nestedArraysEqual in Common.ts

--- problems/common/Common.ts
+++ problems/common/Common.ts
@@ -1,8 +1,11 @@
-function arrayEqual(arr1: any[], arr2: any[]): boolean {
-  if (arr1.length !== arr2.length) return false;
-  return arr1.every((val, i) => {
-    if (Array.isArray(val) && Array.isArray(arr2[i]))
-      return arrayEqual(val, arr2[i]);
-    return val === arr2[i];
-  });
-}
+function arraysEqual(arr1: any[], arr2: any[]) {
+  return (arr1.length === arr2.length
+    && arr1.every((val, pos) => (val === arr2[pos])));
+};
+
+function nestedArraysEqual(arr1: any[], arr2: any[]): boolean {
+  return (arr1.length === arr2.length
+    && arr1.every((val, pos) => (
+      (Array.isArray(val) && Array.isArray(arr2[pos]) && nestedArraysEqual(val, arr2[pos]))
+      || val === arr2[pos])));
+};
