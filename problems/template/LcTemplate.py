d2d6575cbed90a306aa85ff31f0d783efed5f03f
Pin-Xuan She <snakeneedy@gmail.com>
2024-05-05 09:45:20 +0800

    Modifty test function in LcTemplate (py & ts)

--- problems/template/LcTemplate.py
+++ problems/template/LcTemplate.py
@@ -2,7 +2,12 @@ class Solution:
     pass
 
 
-if __name__ == '__main__':
+def main():
     sol = Solution()
-    # assert expected == result, f'expected {expected}, but {result}'
-    pass
+    result = 0
+    expected = 0
+    assert expected == result, f'expected {expected}, but {result}'
+
+
+if __name__ == '__main__':
+    main()
--- problems/template/LcTemplate.ts
+++ problems/template/LcTemplate.ts
@@ -1,6 +1,6 @@
 function main() {
   let result: number = 0, expected: number = 0;
 
-  expected === result || console.trace(`expected ${expected}, but ${result}`);
+  if (expected !== result) new Error(`expected ${expected}, but ${result}`);
 };
 main();
