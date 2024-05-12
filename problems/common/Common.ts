d4cb6ab671964bf8b6b757deecea8c055dd1dd99
Pin-Xuan She <snakeneedy@gmail.com>
2024-05-12 14:48:20 +0800

    Make Common.ts export as a module

--- problems/common/Common.ts
+++ problems/common/Common.ts
@@ -1,11 +1,16 @@
-function arraysEqual(arr1: any[], arr2: any[]) {
+export function arraysEqual(arr1: any[], arr2: any[]) {
   return (arr1.length === arr2.length
     && arr1.every((val, pos) => (val === arr2[pos])));
 };
 
-function nestedArraysEqual(arr1: any[], arr2: any[]): boolean {
+export function nestedArraysEqual(arr1: any[], arr2: any[]): boolean {
   return (arr1.length === arr2.length
     && arr1.every((val, pos) => (
       (Array.isArray(val) && Array.isArray(arr2[pos]) && nestedArraysEqual(val, arr2[pos]))
       || val === arr2[pos])));
+};
+
+export default {
+  arraysEqual,
+  nestedArraysEqual,
 };
